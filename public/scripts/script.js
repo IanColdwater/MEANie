167cdf49833a3252b32934ed7809d168b9ffff62
Ian <shiny@what.local>
2016-09-26 13:34:37 -0500

    fixed script.js formatting and added comments to all

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,34 @@
-var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http ){
-$scope.addRecord = function(){
-event.preventDefault();
-var objectToSend ={
-name: $scope.nameIn,
-location: $scope.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-$scope.nameIn ='';
-$scope.locationIn='';
-};
-$scope.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-$scope.allTheRecords = response;
-console.log( $scope.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
+var myApp = angular.module('myApp', []);
+
+// add new record
+myApp.controller('whereMyPeeps', ['$scope', '$http', function($scope, $http) {
+    $scope.addRecord = function() {
+        event.preventDefault();
+        var objectToSend = {
+            name: $scope.nameIn,
+            location: $scope.locationIn,
+        };
+        $http({
+            method: 'POST',
+            url: '/testPost',
+            data: objectToSend
+        });
+        $scope.nameIn = '';
+        $scope.locationIn = '';
+    };
+
+    // get all the records
+    $scope.getRecords = function(response) {
+        http({
+                method: 'GET',
+                url: '/getRecords',
+            })
+            .success(function(response) {
+                $scope.allTheRecords = response;
+                console.log($scope.allTheRecords);
+            })
+            .error(function(response) {
+                console.log(response.statusText);
+    });
+  };
 }]);
